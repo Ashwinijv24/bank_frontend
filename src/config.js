69f421dd321e51869f35275ed888446ecf8c8765
Ashwinijv24 <your-github-email@example.com>
2026-04-11 17:34:08 +0530

    Update backend URL to bank-app-3-1fn0.onrender.com

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const BASE_URL = process.env.REACT_APP_API_URL
   ? process.env.REACT_APP_API_URL.replace('/api', '')
-  : 'https://bank-app-aoc8.onrender.com';
+  : 'https://bank-app-3-1fn0.onrender.com';
 
 export const API_BASE = BASE_URL;
 export const API_URL  = `${BASE_URL}/api`;
