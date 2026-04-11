5eb2704026b2056798b34828d33fa2fe410463f4
Ashwinijv24 <your-github-email@example.com>
2026-04-11 22:02:18 +0530

    Update config.js with deployment URLs

--- src/config.js
+++ src/config.js
@@ -4,3 +4,6 @@ const BASE_URL = process.env.REACT_APP_API_URL
 
 export const API_BASE = BASE_URL;
 export const API_URL  = `${BASE_URL}/api`;
+
+// Backend: https://bank-app-3-1fn0.onrender.com
+// Frontend: https://enchanting-capybara-9efc5b.netlify.app
